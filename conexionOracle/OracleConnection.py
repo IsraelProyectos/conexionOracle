59743970ab1fb0ccf2fba060336f147a9272f538
Israel Puyol Soriano <israelpuyol@hotmail.com>
2017-09-19 13:03:34 +0200

    dataset con python

--- conexionOracle/OracleConnection.py
+++ conexionOracle/OracleConnection.py
@@ -5,23 +5,37 @@ clr.AddReference("System.Data")
 from System.Data import DataSet
 from System.Data.OleDb import OleDbConnection, OleDbDataAdapter, OleDbCommand
 
+
 class class1(object):
+
+    
     #WORK_SKO/WORK_SKO@bvn002b.bbdo.local/PRDBATCH
-    conStr = "Provider=OraOLEDB.Oracle;Data Source=;User Id=DRUGO73;Password=lokomotiv1970;"
+    conStr = "Provider=MSDAORA.1;Data Source=PRDBATCH;User Id=WORK_SKO;Password=WORK_SKO;"
 
     con = OleDbConnection(conStr)
 
-    query = "SELECT * FROM PERSONAJE"
+    query = "SELECT * FROM EXCEL_SENDER_PARAMETERS"
 
     adapter = OleDbDataAdapter(query, con)
     ds = DataSet()
     con.Open()
 
     adapter.Fill(ds, "t1")
+
+    
+
     for i in range(ds.Tables["t1"].Rows.Count -1):
-        print ds.Tables["t1"].Rows[i][0]
-        print ds.Tables["t1"].Rows[i][1]
-        print ds.Tables["t1"].Rows[i][2]
+        uno = ds.Tables["t1"].Rows[i][0]
+        dos = ds.Tables["t1"].Rows[i][1]
+        tres = ds.Tables["t1"].Rows[i][2]
+
+    def dat(self):
+        for registro in ds:
+            uno = registro
+
+    def saludo(self):
+        return "hola"
+        
 
     con.Close()
 
--- conexionOracle/conexionOracle.py
+++ conexionOracle/conexionOracle.py
@@ -1,11 +1,19 @@
 import wpf
-
+import clr
+import OracleConnection
+clr.AddReference("System.Drawing")
+clr.AddReference("System.Windows.Forms")
+from System.Windows.Forms import Application, Button, Form, Label
 from System.Windows import Application, Window
+
 
 class MyWindow(Window):
     def __init__(self):
+        
         wpf.LoadComponent(self, 'conexionOracle.xaml')
-    
+        oc = OracleConnection.class1()
+        self.label1.Content = "Hola"
 
+        
 if __name__ == '__main__':
     Application().Run(MyWindow())
